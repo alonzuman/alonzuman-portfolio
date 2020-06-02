e466a28f7df6c9807bbe3b2b2df504e9695fc34c
Alon Zuman <alonzuman7@gmail.com>
2020-06-02 16:19:51 +0300

    fixed button colors

--- src/components/Themes.js
+++ src/components/Themes.js
@@ -1,23 +1,22 @@
-const primary = '#ffa7c4';
 const dark = '#2b2a2c';
 
 export const lightTheme = {
   body: '#FFF',
+  primary: '#d23669',
   text: '#363537',
   toggleBorder: 'black',
   paragraph: '#414142',
   background: '#f4f4f4',
   selected: '#f4f4f4',
   dark,
-  primary
 }
 export const darkTheme = {
   body: '#363537',
   text: '#FAFAFA',
+  primary: '#ffa7c4',
   paragraph: '#e4e4e4',
   toggleBorder: 'black',
   background: '#999',
   selected: '#585858',
   dark,
-  primary
 }
